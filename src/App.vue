5b49c93a3a388aabca6972d877cffb0af7b7a2c9
张晓甜 <zhangxt1@tiens.com>
2022-10-20 16:33:09 +0800

    build: master 主版 本

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@
 </template>
 <script setup lang="ts">
 import SvgIcon from '@/components/SvgIcon.vue'
-const testa = ref('1')
-window.webkit.test('xxx')
 </script>
 <style scoped lang="scss">
 .icon-svg {
